65cbabcc61b95f7383f7b9327ece7b00ed0a18d1
Francisco Daines <fdaines@gmail.com>
2023-03-30 17:09:06 -0300

    feature: Implements a new design for coupling analysis

--- frontend/src/components/analysis/tools/dependency-coupling/CouplingDetails.vue
+++ frontend/src/components/analysis/tools/dependency-coupling/CouplingDetails.vue
@@ -6,47 +6,72 @@
     <div class="card-body">
       <ul class="list-group list-group-unbordered mb-3">
         <li class="list-group-item">
-          <b>Package</b>
+          <b>Dependency</b>
           <div class="float-right">{{ result?.dependency }}</div>
         </li>
         <li class="list-group-item">
           <b>Overall Coupling Level</b>
-          <div class="float-right">{{ result?.coupling_level.toLocaleString('en-us', {}) }}</div>
+          <div class="float-right">
+            <div class="badge badge-warning" style="padding: 6px 6px 0 6px;">
+              <h6>{{ result?.coupling_level.toLocaleString('en-us', {}) }}</h6>
+            </div>
+          </div>
         </li>
         <li class="list-group-item">
           <div class="inner-details">
-            <ul class="list-group list-group-unbordered mb-3">
-              <li v-for="pkgCoupling in result?.details" class="list-group-item">
-                <b>{{ pkgCoupling.package }}</b>
-                <div class="float-right">Coupling Level: {{ pkgCoupling.coupling_level.toLocaleString('en-us', {}) }}</div>
-                <div class="col-md-8">
+
+            <div v-for="pkgCoupling in result?.details" class="card card-outline card-default collapsed-card">
+              <div class="card-header">
+                <h3 class="card-title bold">
+                  <small class="badge badge-warning" style="margin-right: 10px;">
+                    Coupling Level: {{ pkgCoupling.coupling_level.toLocaleString('en-us', {}) }}
+                  </small>
+                  {{ pkgCoupling.package }}
+                </h3>
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Open Details">
+                    <i class="fas fa-plus"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body" style="display: none;">
                 <div class="inner-details">
-                  <ul class="list-group list-group-unbordered mb-3">
-                    <li v-for="fileCoupling in pkgCoupling.details" class="list-group-item">
-                        <b>{{ fileCoupling.file }}</b>
-                        <div class="float-right">Coupling Level: {{ fileCoupling.coupling_level.toLocaleString('en-us', {}) }}</div>
-                        <div class="inner-details col-md-8">
-                          <table class="coupling table table-bordered">
-                            <thead>
-                            <tr>
-                              <th style="width:80px;">Line</th>
-                              <th>Details</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr v-for="d in fileCoupling.details">
-                              <td style="text-align: center;">{{ d.line}}</td>
-                              <td>{{  d.details }}</td>
-                            </tr>
-                            </tbody>
-                          </table>
-                        </div>
-                    </li>
-                  </ul>
+                  <div v-for="fileCoupling in pkgCoupling.details" class="card card-outline card-default collapsed-card">
+                    <div class="card-header">
+                      <h3 class="card-title bold">
+                        <small class="badge badge-warning" style="margin-right: 10px;">
+                          Coupling Level: {{ fileCoupling.coupling_level.toLocaleString('en-us', {}) }}
+                        </small>
+                        {{ fileCoupling.file }}
+                      </h3>
+                      <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Open Details">
+                          <i class="fas fa-plus"></i>
+                        </button>
+                      </div>
+                    </div>
+                    <div class="card-body" style="display: none;">
+                      <div class="inner-details">
+                        <table class="coupling table table-bordered">
+                          <thead>
+                          <tr>
+                            <th style="width:80px;">Line</th>
+                            <th>Details</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          <tr v-for="d in fileCoupling.details">
+                            <td style="text-align: center;">{{ d.line}}</td>
+                            <td>{{  d.details }}</td>
+                          </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                </div>
-              </li>
-            </ul>
+              </div>
+            </div>
           </div>
         </li>
       </ul>
@@ -57,6 +82,7 @@
 <script lang="ts">
 import {PropType} from "vue";
 import {coupling} from "../../../../../wailsjs/go/models";
+import {BrowserOpenURL} from "../../../../../wailsjs/runtime";
 
 export default {
   name: "DependencyCouplingDetails",
@@ -64,6 +90,11 @@ export default {
     result: {
       required: true,
       type: Object as PropType<coupling.DependencyCoupling>
+    }
+  },
+  methods: {
+    openDetails(pkg: string) {
+      console.log("Open Details for: " + pkg)
     }
   }
 }
@@ -78,6 +109,6 @@ export default {
   text-align: center;
 }
 .coupling td, .coupling th {
-  padding: 0;
+  padding: 0 0 0 10px;
 }
 </style>
