474f621aeddb4c8e0b2564bcfcd7e30aaad501b7
Francisco Daines <fdaines@gmail.com>
2023-08-31 16:41:39 -0400

    fix: Changes dependency graph library, now using vis-network

--- frontend/src/components/analysis/tools/dependency-graph/DependencyGraph.vue
+++ frontend/src/components/analysis/tools/dependency-graph/DependencyGraph.vue
@@ -11,16 +11,35 @@
     <div class="card-body">
       <div class="row">
         <div class="col-9">
-          <v-network-graph
-              class="graph"
-              :nodes="nodes"
-              :edges="edges"
-              :layouts="layouts"
-              :configs="configs"
-          />
+          <div id="dependency-graph"></div>
         </div>
         <div class="col-3">
           <legend-box />
+          <div class="card card-primary card-outline">
+            <div class="card-header">
+              <h3 class="card-title">Package Details</h3>
+            </div>
+            <div class="card-body package-details">
+              <ul class="list-group list-group-unbordered mb-3">
+                <li class="list-group-item">
+                  <b>Selected Package</b>
+                  <div>{{ selectedNode.package }}</div>
+                </li>
+                <li class="list-group-item">
+                  <b>Depends on Packages</b>
+                  <ul>
+                    <li v-for="d in selectedNode.dependencies">{{ d }}</li>
+                  </ul>
+                </li>
+                <li class="list-group-item">
+                  <b>Dependant Packages</b>
+                  <ul>
+                    <li v-for="d in selectedNode.dependants">{{ d }}</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -28,66 +47,29 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from "vue";
-import { Node, Edge, Layouts, VNetworkGraphInstance } from "v-network-graph";
-//@ts-ignore
-import dagre from "dagre/dist/dagre.min.js"
+import {defineComponent} from "vue";
+import * as vis from "vis-network/peer";
 
 import {getSelectedProject} from "../../../../utils/storage";
 import {dependency, project} from "../../../../../wailsjs/go/models";
 import {GetDependencyGraph} from "../../../../../wailsjs/go/main/Api";
 import LegendBox from "./LegendBox.vue";
 
-const nodes: { [key: string]: Node } = {}
-const edges: { [key: string]: Edge } = {}
-const layouts: Layouts = { nodes: {}}
-
-const graph = ref<VNetworkGraphInstance>()
+const nodeEdges: any[] = []
+const nodesArray: vis.Node[] = []
+const edgesArray: vis.Edge[] = []
 
 export default defineComponent( {
   name: "DependencyGraph",
   components: { LegendBox },
   data() {
     return {
-      nodes: nodes,
-      edges: edges,
-      layouts: layouts,
-      configs: {
-        node: {
-          normal: {
-            type: "circle",
-            radius: (node: any) => node.size, // Use the value of each node object
-            color: (node: any) => node.color,
-          },
-          hover: {
-            radius: (node: any) => node.size + 2,
-            color: (node: any) => node.color,
-          },
-          selectable: true,
-          label: {
-            visible: true,
-            fontFamily: undefined,
-            fontSize: 11,
-            lineHeight: 1.1,
-            color: "#000000",
-            margin: 4,
-            direction: "south",
-            text: "name",
-          },
-          focusring: {
-            color: "darkgray",
-          },
-        },
-        edge: {
-          marker: {
-            source: {
-              type: "none",
-            },
-            target: {
-              type: "arrow"
-            }
-          }
-        }
+      nodes: nodesArray,
+      edges: edgesArray,
+      selectedNode: {
+        package: "",
+        dependencies: [],
+        dependants: []
       },
       project: undefined as project.ProjectInfo | undefined,
       result: undefined as dependency.ModuleDependencyGraph | undefined,
@@ -98,107 +80,150 @@ export default defineComponent( {
     mapToNetworkGraph(dg: dependency.ModuleDependencyGraph) {
       this.mapNodes(dg)
       this.mapEdges(dg)
-      this.layout("TB")
     },
     mapEdges(dg: dependency.ModuleDependencyGraph) {
       for(let origin in dg.relations){
         for(let idx in dg.relations[origin]){
-          this.edges[origin+"_"+dg.relations[origin][idx]] = { source: origin, target: dg.relations[origin][idx] }
+          edgesArray.push({
+            from: origin,
+            to: dg.relations[origin][idx],
+            color: {
+              color: "#80d6ff",
+              highlight: "#ff0000"
+            },
+            arrows: {
+              to: true
+            },
+            chosen: true
+          })
+          const idx2 = nodeEdges.findIndex((ne) => ne.name === origin)
+          if (idx2 >= 0) {
+            nodeEdges[idx2].count++
+          } else {
+            nodeEdges.push({ name: origin, count: 1 })
+          }
         }
       }
     },
     mapNodes(dg: dependency.ModuleDependencyGraph){
+      const selectNode = (node: string) => {
+        function getDependencies(edgesArray: vis.Edge[], node: string) {
+          const dependencies: string[] = []
+          edgesArray.forEach(edge => {
+            if (edge.from == node && edge.to != undefined) {
+              dependencies.push(edge.to+"")
+            }
+          })
+          return dependencies
+        }
+        function getDependants(edgesArray: vis.Edge[], node: string) {
+          const dependants: string[] = []
+          edgesArray.forEach(edge => {
+            if (edge.to == node && edge.from != undefined) {
+              dependants.push(edge.from+"")
+            }
+          })
+          return dependants
+        }
+
+        this.selectedNode = {
+          package: node,
+          dependencies: getDependencies(edgesArray, node),
+          dependants: getDependants(edgesArray, node)
+        }
+      }
       for(let n in dg.internal){
-        this.nodes[dg.internal[n]] = { name: dg.internal[n], size: 8, color: "#80d6ff" }
-      }
-      for(let n in dg.external){
-        this.nodes[dg.external[n]] = { name: dg.external[n], size: 8, color: "#f06868" }
-      }
-      for(let n in dg.standard){
-        this.nodes[dg.standard[n]] = { name: dg.standard[n], size: 8, color: "#edf798" }
+        nodesArray.push({
+          id: dg.internal[n],
+          label: dg.internal[n],
+          color: "#80d6ff",
+          shape: 'box',
+          chosen: {
+            label: () => selectNode(dg.internal[n])
+          }
+        })
       }
       for(let n in dg.organization){
-        this.nodes[dg.organization[n]] = { name: dg.organization[n], size: 8, color: "#fab57a" }
+        nodesArray.push({
+          id: dg.organization[n],
+          label: dg.organization[n],
+          color: "#fab57a",
+          shape: 'box',
+          chosen: {
+            label: () => selectNode(dg.organization[n])
+          }
+        })
+      }
+      for(let n in dg.external){
+        nodesArray.push({
+          id: dg.external[n],
+          label: dg.external[n],
+          color: "#f06868",
+          shape: 'box',
+          chosen: {
+            label: () => selectNode(dg.external[n])
+          }
+        })
+      }
+      for(let n in dg.standard){
+        nodesArray.push({
+          id: dg.standard[n],
+          label: dg.standard[n],
+          color: "#edf798",
+          shape: 'box',
+          chosen: {
+            label: () => selectNode(dg.standard[n])
+          }
+        })
       }
       this.nodesSize = dg.internal.length + dg.external.length + dg.standard.length
       if(dg.organization != null) this.nodesSize += dg.organization.length
-    },
-    layout(direction: "TB" | "LR") {
-      if (Object.keys(this.nodes).length <= 1 || Object.keys(this.edges).length == 0) {
-        return
-      }
-
-      // convert graph
-      // ref: https://github.com/dagrejs/dagre/wiki
-      const g = new dagre.graphlib.Graph()
-      // Set an object for the graph label
-      g.setGraph({
-        rankdir: direction,
-        nodesep: this.nodesSize * 3,
-        edgesep: this.nodesSize,
-        ranksep: this.nodesSize * 2,
-      })
-      // Default to assigning a new object as a label for each new edge.
-      g.setDefaultEdgeLabel(() => ({}))
-
-      // Add nodes to the graph. The first argument is the node id. The second is
-      // metadata about the node. In this case we're going to add labels to each of
-      // our nodes.
-      Object.entries(this.nodes).forEach(([nodeId, node]) => {
-        g.setNode(nodeId, { label: node.name, width: this.nodesSize, height: this.nodesSize })
-      })
-
-      // Add edges to the graph.
-      Object.values(this.edges).forEach(edge => {
-        g.setEdge(edge.source, edge.target)
-      })
-
-      dagre.layout(g)
-
-      const box: Record<string, number | undefined> = {}
-      g.nodes().forEach((nodeId: string) => {
-        // update node position
-        const x = g.node(nodeId).x
-        const y = g.node(nodeId).y
-        this.layouts.nodes[nodeId] = { x, y }
-
-        // calculate bounding box size
-        box.top = box.top ? Math.min(box.top, y) : y
-        box.bottom = box.bottom ? Math.max(box.bottom, y) : y
-        box.left = box.left ? Math.min(box.left, x) : x
-        box.right = box.right ? Math.max(box.right, x) : x
-      })
-
-      const graphMargin = this.nodesSize * 2
-      const viewBox = {
-        top: (box.top ?? 0) - graphMargin,
-        bottom: (box.bottom ?? 0) + graphMargin,
-        left: (box.left ?? 0) - graphMargin,
-        right: (box.right ?? 0) + graphMargin,
-      }
-      graph.value?.setViewBox(viewBox)
     }
   },
   async mounted() {
-    this.nodes = nodes
-    this.edges = edges
-    this.layouts = layouts
-    this.nodesSize = 0
-
     this.project = getSelectedProject()
     if(this.project !== undefined){
       const dg = await GetDependencyGraph(this.project)
       this.mapToNetworkGraph(dg)
     }
+
+    const graphData: vis.Data = {
+      nodes: nodesArray,
+      edges: edgesArray
+    };
+
+    const options: vis.Options = {
+      manipulation: false,
+      height: '100%',
+      width: '100%',
+      layout: {
+        hierarchical: {
+          enabled: true,
+          levelSeparation: 80
+        }
+      },
+      physics: {
+        hierarchicalRepulsion: {
+          nodeDistance: 300,
+          avoidOverlap: 1
+        }
+      }
+    }
+    const container = document.getElementById('dependency-graph');
+    new vis.Network(container!, graphData, options);
   }
 })
 </script>
 
 <style scoped>
-.graph {
+#dependency-graph {
   width: 100%;
   height: 600px;
   border: 1px solid black;
   border-radius: 5px;
+}
+
+.package-details {
+  font-size: 12px;
 }
 </style>
--- frontend/src/components/analysis/tools/metrics/Types.vue
+++ frontend/src/components/analysis/tools/metrics/Types.vue
@@ -1,5 +1,43 @@
 <script lang="ts" setup>
 const props = defineProps(['data'])
+
+const previousData = {
+  packages: 10,
+  source_files: 12,
+  structs: 5,
+  interfaces: 0,
+  functions: 7,
+  methods: 3,
+  variables: 7,
+  constants: 38
+}
+const resolveIcon = (current: number, previous:number): string => {
+  if (current == previous) {
+    return "fa-solid fa-circle"
+  }
+  if (current > previous) {
+    return "fa-solid fa-circle-up"
+  }
+  return "fa-solid fa-circle-down"
+}
+const resolveStyle = (current: number, previous:number): string => {
+  if (current == previous) {
+    return "color: #f1d104;"
+  }
+  if (current > previous) {
+    return "color: #06a741;"
+  }
+  return "color: #df0707;"
+}
+const resolveHint = (current: number, previous:number): string => {
+  if (current == previous) {
+    return "No changes since last analysis"
+  }
+  if (current > previous) {
+    return `+${current-previous} since last analysis`
+  }
+  return `-${previous-current} since last analysis`
+}
 </script>
 
 <template>
@@ -18,35 +56,91 @@ const props = defineProps(['data'])
       <ul v-else class="list-group list-group-unbordered mb-3">
         <li class="list-group-item">
           <b>Packages</b>
-          <div class="float-right">{{ props.data.packages.toLocaleString('en-us', {}) }}</div>
+          <div class="float-right">
+            {{ props.data.packages.toLocaleString('en-us', {}) }}
+            <i v-if="previousData != undefined"
+              :class="resolveIcon(props.data.packages, previousData.packages)"
+              :style="resolveStyle(props.data.packages, previousData.packages)"
+              :title="resolveHint(props.data.packages, previousData.packages)"
+            ></i>
+          </div>
         </li>
         <li class="list-group-item">
           <b>Source Files</b>
-          <div class="float-right">{{ props.data.source_files.toLocaleString('en-us', {}) }}</div>
+          <div class="float-right">
+            {{ props.data.source_files.toLocaleString('en-us', {}) }}
+            <i v-if="previousData != undefined"
+              :class="resolveIcon(props.data.source_files, previousData.source_files)"
+              :style="resolveStyle(props.data.source_files, previousData.source_files)"
+              :title="resolveHint(props.data.source_files, previousData.source_files)"
+            ></i>
+          </div>
         </li>
         <li class="list-group-item">
           <b>Structs</b>
-          <div class="float-right">{{ props.data.structs.toLocaleString('en-us', {}) }}</div>
+          <div class="float-right">
+            {{ props.data.structs.toLocaleString('en-us', {}) }}
+            <i v-if="previousData != undefined"
+              :class="resolveIcon(props.data.structs, previousData.structs)"
+              :style="resolveStyle(props.data.structs, previousData.structs)"
+              :title="resolveHint(props.data.structs, previousData.structs)"
+            ></i>
+          </div>
         </li>
         <li class="list-group-item">
           <b>Interfaces</b>
-          <div class="float-right">{{ props.data.interfaces.toLocaleString('en-us', {}) }}</div>
+          <div class="float-right">
+            {{ props.data.interfaces.toLocaleString('en-us', {}) }}
+            <i v-if="previousData != undefined"
+              :class="resolveIcon(props.data.interfaces, previousData.interfaces)"
+              :style="resolveStyle(props.data.interfaces, previousData.interfaces)"
+              :title="resolveHint(props.data.interfaces, previousData.interfaces)"
+            ></i>
+          </div>
         </li>
         <li class="list-group-item">
           <b>Functions</b>
-          <div class="float-right">{{ props.data.functions.toLocaleString('en-us', {}) }}</div>
+          <div class="float-right">
+            {{ props.data.functions.toLocaleString('en-us', {}) }}
+            <i v-if="previousData != undefined"
+              :class="resolveIcon(props.data.functions, previousData.functions)"
+              :style="resolveStyle(props.data.functions, previousData.functions)"
+              :title="resolveHint(props.data.functions, previousData.functions)"
+            ></i>
+          </div>
         </li>
         <li class="list-group-item">
           <b>Methods</b>
-          <div class="float-right">{{ props.data.methods.toLocaleString('en-us', {}) }}</div>
+          <div class="float-right">
+            {{ props.data.methods.toLocaleString('en-us', {}) }}
+            <i v-if="previousData != undefined"
+              :class="resolveIcon(props.data.methods, previousData.methods)"
+              :style="resolveStyle(props.data.methods, previousData.methods)"
+              :title="resolveHint(props.data.methods, previousData.methods)"
+            ></i>
+          </div>
         </li>
         <li class="list-group-item">
           <b>Variables</b>
-          <div class="float-right">{{ props.data.variables.toLocaleString('en-us', {}) }}</div>
+          <div class="float-right">
+            {{ props.data.variables.toLocaleString('en-us', {}) }}
+            <i v-if="previousData != undefined"
+              :class="resolveIcon(props.data.variables, previousData.variables)"
+              :style="resolveStyle(props.data.variables, previousData.variables)"
+              :title="resolveHint(props.data.variables, previousData.variables)"
+            ></i>
+          </div>
         </li>
         <li class="list-group-item">
           <b>Constants</b>
-          <div class="float-right">{{ props.data.constants.toLocaleString('en-us', {}) }}</div>
+          <div class="float-right">
+            {{ props.data.constants.toLocaleString('en-us', {}) }}
+            <i v-if="previousData != undefined"
+              :class="resolveIcon(props.data.constants, previousData.constants)"
+              :style="resolveStyle(props.data.constants, previousData.constants)"
+              :title="resolveHint(props.data.constants, previousData.constants)"
+            ></i>
+          </div>
         </li>
       </ul>
     </div>
