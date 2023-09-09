8273a987851cd473daea880e90757d0b640371b8
Francisco Daines <fdaines@gmail.com>
2023-09-09 12:31:07 -0300

    fix: Removes wrong dependency

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,10 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import 'jquery'
 import { router } from './router.js'
-// @ts-ignore
-import VueHighlightJS from 'vue3-highlightjs'
 
 createApp(App)
     .use(router)
-    .use(VueHighlightJS)
     .mount('#app')
